c53fd5270d8bee3d3643167a7dace240bf80db3b
David Saltares <david.saltares@gmail.com>
2022-09-30 18:56:46 +0300

    feat: set Inter font

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
