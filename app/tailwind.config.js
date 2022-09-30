d58ba3bd4b86f8a0c5f2a5cd179789353724f8c4
David Saltares <david.saltares@gmail.com>
2022-09-30 18:58:30 +0300

    feat: add storybook

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './.storybook/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
