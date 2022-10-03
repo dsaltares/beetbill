66dd7db4c8541c21a796f812298ba7c4f5dc5402
David Saltares <david.saltares@gmail.com>
2022-10-03 18:00:21 +0300

    chore: adjust size in verify page

--- app/src/pages/auth/verify.page.tsx
+++ app/src/pages/auth/verify.page.tsx
@@ -4,7 +4,7 @@ import Card from '@components/Card';
 const Verify = () => (
   <BlankLayout>
     <Card>
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-10">
         <h1 className="text-2xl font-bold text-center">Got it! Email sent.</h1>
         <p className="text-base">
           Check your email for a button that will log you in.
