0aad9204a5665293f3666db457d19b5910f4ab89
David Saltares <david.saltares@gmail.com>
2022-10-04 15:24:55 +0300

    test: add router capabilities to wrapped render

--- app/src/lib/testing.tsx
+++ app/src/lib/testing.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-console */
 /* eslint-disable import/export */
-import React, { type ReactNode } from 'react';
+import React, { useState, type PropsWithChildren } from 'react';
 import { render } from '@testing-library/react';
 import { SessionProvider } from 'next-auth/react';
+import type { NextRouter } from 'next/router';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 import type { Session } from 'next-auth';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -13,35 +15,62 @@ const customRender = (
 
 type ProviderData = {
   session?: Session;
+  router?: Partial<NextRouter>;
 };
 
-type NewType = ReactNode;
-
-type ChildrenProps = {
-  children?: NewType;
-};
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-    },
-  },
-  logger: {
-    log: console.log,
-    warn: console.warn,
-    error: () => {},
-  },
-});
-
-const createProviders = ({ session }: ProviderData) => {
-  const Providers = ({ children }: ChildrenProps) => (
-    <QueryClientProvider client={queryClient}>
-      <SessionProvider session={session}>{children}</SessionProvider>
-    </QueryClientProvider>
-  );
+const createProviders = ({ session, router }: ProviderData) => {
+  const Providers = ({ children }: PropsWithChildren) => {
+    const [queryClient] = useState(
+      () =>
+        new QueryClient({
+          defaultOptions: {
+            queries: {
+              retry: false,
+            },
+          },
+          logger: {
+            log: console.log,
+            warn: console.warn,
+            error: () => {},
+          },
+        })
+    );
+    return (
+      <RouterContext.Provider value={{ ...mockRouter, ...(router || {}) }}>
+        <QueryClientProvider client={queryClient}>
+          <SessionProvider session={session}>{children}</SessionProvider>
+          {children}
+        </QueryClientProvider>
+      </RouterContext.Provider>
+    );
+  };
 
   return Providers;
+};
+
+export const mockRouter: NextRouter = {
+  basePath: '',
+  pathname: '/',
+  route: '/',
+  query: {},
+  asPath: '/',
+  back: jest.fn(),
+  beforePopState: jest.fn(),
+  prefetch: () => Promise.resolve(),
+  push: jest.fn(),
+  reload: jest.fn(),
+  replace: jest.fn(),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isReady: true,
+  defaultLocale: 'en',
+  domainLocales: [],
+  isPreview: false,
 };
 
 export * from '@testing-library/react';
