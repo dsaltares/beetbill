6c74b83aa2958681832578afc5ce4963ac7d7ff5
David Saltares <david.saltares@gmail.com>
2022-10-06 15:53:26 +0300

    fix: avoid layout shift in Sidebar

--- app/src/components/Sidebar/Sidebar.tsx
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
 import IconButton from '@components/IconButton';
 import Logo from '../Logo';
@@ -11,46 +10,35 @@ type SidebarProps = {
   onClose: () => void;
 };
 
-const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
-  const ref = useRef<HTMLBaseElement>(null);
-  const [width, setWidth] = useState(0);
-  useEffect(() => {
-    setWidth(ref.current?.offsetWidth ?? 0);
-  }, [ref, setWidth]);
-  return (
-    <>
-      <aside
-        ref={ref}
-        className={cn(
-          'flex flex-col justify-between p-4 w-full h-full fixed z-20 top-0 md:w-[242px] bg-violet-900 gap-10 ease-in-out duration-300',
-          {
-            '-translate-x-full': !isOpen,
-            'md:flex md:translate-x-0': !isOpen,
-          }
-        )}
-      >
-        <div className="flex flex-col gap-10">
-          <div className="flex items-center justify-between">
-            <div className="block md:hidden">
-              <IconButton
-                variant="secondary"
-                label="close menu"
-                icon={faXmark}
-                onClick={onClose}
-              />
-            </div>
-            <Logo variant="secondary" />
+const Sidebar = ({ isOpen, onClose }: SidebarProps) => (
+  <>
+    <aside
+      className={cn(
+        'flex flex-col justify-between p-4 w-full h-full fixed z-20 top-0 md:w-[242px] bg-violet-900 gap-10 ease-in-out duration-300',
+        {
+          '-translate-x-full': !isOpen,
+          'md:flex md:translate-x-0': !isOpen,
+        }
+      )}
+    >
+      <div className="flex flex-col gap-10">
+        <div className="flex items-center justify-between">
+          <div className="block md:hidden">
+            <IconButton
+              variant="secondary"
+              label="close menu"
+              icon={faXmark}
+              onClick={onClose}
+            />
           </div>
-          <NavLinks />
+          <Logo variant="secondary" />
         </div>
-        <UserMenu />
-      </aside>
-      <div
-        className="hidden md:flex h-full w-full"
-        style={{ maxWidth: width }}
-      ></div>
-    </>
-  );
-};
+        <NavLinks />
+      </div>
+      <UserMenu />
+    </aside>
+    <div className="hidden md:flex h-full md:w-[242px]"></div>
+  </>
+);
 
 export default Sidebar;
