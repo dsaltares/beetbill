f6af6a3cb37a93eb4e65a998fcdeb621e548f274
David Saltares <david.saltares@gmail.com>
2022-10-06 11:37:37 +0300

    test: createProduct tests

---
+++ app/src/tests-integration/products/createProduct.test.ts
@@ -0,0 +1,44 @@
+import type { Company, User } from '@prisma/client';
+import type { Session } from 'next-auth';
+import cuid from 'cuid';
+import prisma from '@server/prisma';
+import { createTestCompany, createTestUser } from '../testData';
+import { createProduct } from '@server/products/createProduct';
+
+let user: User;
+let company: Company;
+let session: Session;
+
+describe('createProduct', () => {
+  beforeEach(async () => {
+    user = await createTestUser();
+    company = await createTestCompany(user.id);
+    session = { userId: user.id, companyId: company.id, expires: '' };
+  });
+
+  it('creates a product for the company in the session', async () => {
+    const input = {
+      name: 'Test Product',
+    };
+    const result = await createProduct({
+      ctx: { session },
+      input,
+    });
+    const dbCustomer = await prisma.product.findUnique({
+      where: { id: result.id },
+    });
+    expect(result).toMatchObject(input);
+    expect(result).toEqual(dbCustomer);
+  });
+
+  it('throws when the company does not exist', async () => {
+    await expect(
+      createProduct({
+        ctx: { session: { ...session, companyId: 'invalid_company' } },
+        input: {
+          name: 'Test Product',
+        },
+      })
+    ).rejects.toThrow();
+  });
+});
