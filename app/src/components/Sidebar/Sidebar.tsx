a4e17c6e92e54af4db75f638b839a81d5cf744d0
David Saltares <david.saltares@gmail.com>
2022-10-06 16:51:15 +0300

    fix: Sidebar layout (I think)

--- app/src/components/Sidebar/Sidebar.tsx
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -37,7 +37,7 @@ const Sidebar = ({ isOpen, onClose }: SidebarProps) => (
       </div>
       <UserMenu />
     </aside>
-    <div className="hidden md:flex h-full md:w-[242px]"></div>
+    <div className="hidden md:block h-full w-full max-w-[242px]"></div>
   </>
 );
 
