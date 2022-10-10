00f4bca1a55d3c990f93795b7192ef44b93842cc
David Saltares <david.saltares@gmail.com>
2022-10-10 14:06:41 +0300

    fix: 59 - TextField style issues

--- app/src/components/TextField.tsx
+++ app/src/components/TextField.tsx
@@ -9,59 +9,68 @@ type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & {
 };
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ label, tip, error, id, className, disabled, ...props }, ref) => (
-    <div className="w-full">
-      {label && (
-        <div className="mb-2">
-          <label
-            htmlFor={id}
-            className={cn('text-sm font-medium', {
+  ({ label, tip, error, id, className, disabled, ...props }, ref) => {
+    const showError = !!error && !disabled;
+    const showTip = !showError && !!tip;
+
+    return (
+      <div className="w-full">
+        {label && (
+          <div className="mb-2">
+            <label
+              htmlFor={id}
+              className={cn('text-sm font-medium', {
+                'text-zinc-900': !error && !disabled,
+                'text-red-600': !!error && !disabled,
+                'text-zinc-400': !!disabled,
+              })}
+            >
+              {label}
+            </label>
+          </div>
+        )}
+        <input
+          id={id}
+          ref={ref}
+          disabled={disabled}
+          {...props}
+          className={cn(
+            'border text-sm rounded-lg focus-ring block w-full p-2.5',
+            className,
+            {
+              'bg-gray-50': !disabled,
+              'bg-gray-100': !!disabled,
+              'border-gray-300': !error || !!disabled,
+              'border-red-600': !!error && !disabled,
               'text-zinc-900': !error && !disabled,
               'text-red-600': !!error && !disabled,
               'text-zinc-400': !!disabled,
-            })}
-          >
-            {label}
-          </label>
-        </div>
-      )}
-      <input
-        id={id}
-        ref={ref}
-        disabled={disabled}
-        {...props}
-        className={cn(
-          'border text-sm rounded-lg focus-ring block w-full p-2.5',
-          className,
-          {
-            'bg-gray-50': !disabled,
-            'bg-gray-100': !!disabled,
-            'border-gray-300': !error,
-            'border-red-600': !!error,
-            'text-zinc-900': !error && !disabled,
-            'text-red-600': !!error && !disabled,
-            'text-zinc-400': !!disabled,
-          }
+            }
+          )}
+        />
+        {showTip && (
+          <div className="mt-2">
+            <span
+              className={cn('text-sm ', {
+                'text-zinc-900': !disabled,
+                'text-zinc-400': !!disabled,
+              })}
+            >
+              {tip}
+            </span>
+          </div>
         )}
-      />
-      {!error && tip && (
-        <div className="mt-2">
-          <span role="alert" className="text-sm text-zinc-900">
-            {tip}
-          </span>
-        </div>
-      )}
-      {error && (
-        <div className="mt-2">
-          <span role="alert" className="text-sm text-red-600">
-            {error}
-          </span>
-        </div>
-      )}
-    </div>
-  )
+        {showError && (
+          <div className="mt-2">
+            <span role="alert" className="text-sm text-red-600">
+              {error}
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
 );
-
 TextField.displayName = 'TextField';
 
 export default TextField;
