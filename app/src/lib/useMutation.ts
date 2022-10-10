066ff07504933e40ee5eca6ee629fd229a251a98
David Saltares <david.saltares@gmail.com>
2022-10-10 18:15:33 +0300

    feat: 61 - show toast error on mutation error

--- app/src/lib/useMutation.ts
+++ app/src/lib/useMutation.ts
@@ -3,18 +3,22 @@ import {
   useQueryClient,
   type QueryKey,
 } from '@tanstack/react-query';
+import type { TRPCError } from '@trpc/server';
 import { type Draft, produce } from 'immer';
+import toast from '@components/Toast';
 
 type UseMutationArgs<Input, Cache> = {
   cacheKey: QueryKey;
   mutationFn: (input: Input) => Promise<unknown>;
   cacheUpdater: (cache: Draft<Cache>, input: Input) => void;
+  errorMessage?: (error: TRPCError) => string;
 };
 
 const useMutation = <Input, Cache>({
   cacheKey,
   mutationFn,
   cacheUpdater,
+  errorMessage,
 }: UseMutationArgs<Input, Cache>) => {
   const queryClient = useQueryClient();
   return useMutationBase(mutationFn, {
@@ -37,10 +41,16 @@ const useMutation = <Input, Cache>({
 
       return {};
     },
-    onError: async (_error, _args, context) => {
+    onError: async (_error: TRPCError, _args, context) => {
       if (context?.previousData) {
         queryClient.setQueryData(cacheKey, context.previousData);
       }
+      const message = errorMessage ? errorMessage(_error) : _error.message;
+      toast({
+        color: 'danger',
+        variant: 'light',
+        message,
+      });
     },
     onSettled: () => queryClient.invalidateQueries(cacheKey),
   });
