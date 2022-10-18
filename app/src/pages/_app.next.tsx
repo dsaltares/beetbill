b87c93ffe68a58accc7ae4f2878cfa7581e4f257
David Saltares <david.saltares@gmail.com>
2022-10-18 09:44:42 +0300

    fix: session in AppProps type issue

--- app/src/pages/_app.next.tsx
+++ app/src/pages/_app.next.tsx
@@ -2,11 +2,18 @@ import '../styles/globals.css';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import type { AppProps } from 'next/app';
+import type { AppProps as BaseAppProps } from 'next/app';
+import type { Session } from 'next-auth';
 import Providers from '@components/Providers';
 import Layout from '@components/Layout';
 
 config.autoAddCss = false;
+
+type AppProps = BaseAppProps & {
+  pageProps: BaseAppProps['pageProps'] & {
+    session?: Session;
+  };
+};
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
   <Providers session={session}>
