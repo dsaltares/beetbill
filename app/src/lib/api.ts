a3b09a6e2a24207eaa098a3513c06354d8bcf10c
David Saltares <david.saltares@gmail.com>
2022-10-21 12:18:33 +0300

    chore: log some env variables to debug issue

--- app/src/lib/api.ts
+++ app/src/lib/api.ts
@@ -2,6 +2,17 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '@server/router';
 
 const getBaseUrl = (): string => {
+  // eslint-disable-next-line no-console
+  console.log('process.env.VERCEL_URL', process.env.VERCEL_URL);
+  // eslint-disable-next-line no-console
+  console.log('process.env.NEXTAUTH_URL', process.env.NEXTAUTH_URL);
+  // eslint-disable-next-line no-console
+  console.log('all env variables', JSON.stringify(process.env, null, 2));
+  // eslint-disable-next-line no-console
+  console.log(
+    'process.env.NEXT_PUBLIC_VERCEL_URL',
+    process.env.NEXT_PUBLIC_VERCEL_URL
+  );
   if (process.env.VERCEL_URL) {
     return `https://${process.env.VERCEL_URL}`;
   }
