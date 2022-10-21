9685bd300dfeafc78c79d87ae9ea3370b0c1d608
David Saltares <david.saltares@gmail.com>
2022-10-21 10:13:37 +0300

    feat: 103 - add adornments to TextField (#105)

--- app/src/components/TextField.tsx
+++ app/src/components/TextField.tsx
@@ -6,10 +6,26 @@ type TextFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label?: string;
   error?: string;
   tip?: string;
+  startAdornment?: React.ReactNode;
+  endAdornment?: React.ReactNode;
 };
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ label, tip, error, id, className, disabled, required, ...props }, ref) => {
+  (
+    {
+      label,
+      tip,
+      error,
+      startAdornment = null,
+      endAdornment = null,
+      id,
+      className,
+      disabled,
+      required,
+      ...props
+    },
+    ref
+  ) => {
     const showError = !!error && !disabled;
     const showTip = !showError && !!tip;
 
@@ -31,26 +47,39 @@ const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
             </label>
           </div>
         )}
-        <input
-          id={id}
-          ref={ref}
-          disabled={disabled}
-          required={required}
-          {...props}
-          className={cn(
-            'border text-sm rounded-lg focus-ring block w-full p-2 placeholder:text-zinc-400',
-            className,
-            {
-              'bg-gray-50': !disabled,
-              'bg-gray-100': !!disabled,
-              'border-gray-300': !error || !!disabled,
-              'border-red-600': !!error && !disabled,
-              'text-zinc-900': !error && !disabled,
-              'text-red-600': !!error && !disabled,
-              'text-zinc-400 cursor-not-allowed': !!disabled,
-            }
+        <div
+          className={cn('flex items-center border text-sm rounded-lg w-full', {
+            'bg-zinc-50': !disabled,
+            'bg-zinc-100': !!disabled,
+            'border-zinc-300': !error || !!disabled,
+            'border-red-600': !!error && !disabled,
+            'text-zinc-900': !error && !disabled,
+            'text-red-600': !!error && !disabled,
+            'text-zinc-400 cursor-not-allowed': !!disabled,
+          })}
+        >
+          {startAdornment && (
+            <div className="text-zinc-500 border-r border-zinc-300 p-2">
+              {startAdornment}
+            </div>
           )}
-        />
+          <input
+            id={id}
+            ref={ref}
+            disabled={disabled}
+            required={required}
+            {...props}
+            className={cn(
+              'w-full placeholder:text-zinc-400 rounded-lg focus-ring p-2',
+              className
+            )}
+          />
+          {endAdornment && (
+            <div className="text-zinc-500 border-l border-zinc-300 p-2">
+              {endAdornment}
+            </div>
+          )}
+        </div>
         {showTip && (
           <div className="mt-1">
             <span
