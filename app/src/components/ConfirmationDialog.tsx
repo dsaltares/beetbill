91ea039d5ad888631e1a069d1650724a647dfce4
David Saltares <david.saltares@gmail.com>
2022-10-25 23:53:35 +0200

    fix: ConfirmationDialog appearing below Sidebar

--- app/src/components/ConfirmationDialog.tsx
+++ app/src/components/ConfirmationDialog.tsx
@@ -33,7 +33,7 @@ const ConfirmationDialog = ({
   }, [onConfirm, onClose]);
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
+      <Dialog as="div" className="relative z-30" onClose={onClose}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
