0a306bf652976dedd515136398474be57b3b1b5b
David Saltares <david.saltares@gmail.com>
2022-10-25 21:24:09 +0200

    feat: 97 - client list screen (#127)

---
+++ app/src/components/ClientsTable.stories.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { Customer } from '@server/customers/types';
+import ClientsTable from './ClientsTable';
+export default {
+  title: 'ClientsTable',
+  component: ClientsTable,
+  argTypes: {
+    onDelete: { action: 'edit' },
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof ClientsTable>;
+
+const clients: Customer[] = [
+  {
+    id: 'client_1',
+    name: 'Client 1',
+    number: '1',
+    vatNumber: '1',
+    firstName: 'Ada',
+    lastName: 'Lovelace',
+    email: 'ada@company.com',
+    country: 'client_country',
+    address: 'client_address',
+    postCode: 'client_post_code',
+    city: 'client_city',
+    paymentTerms: 7,
+    companyId: 'company_1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'client_2',
+    name: 'Client 2',
+    number: '2',
+    vatNumber: '2',
+    firstName: null,
+    lastName: null,
+    email: 'nikola@company.com',
+    country: 'client_country',
+    address: 'client_address',
+    postCode: 'client_post_code',
+    city: 'client_city',
+    paymentTerms: 30,
+    companyId: 'company_1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 'client_3',
+    name: 'Client 3',
+    number: '3',
+    vatNumber: '3',
+    firstName: 'Alan',
+    lastName: 'Turin',
+    email: 'alan@company.com',
+    country: 'client_country',
+    address: 'client_address',
+    postCode: 'client_post_code',
+    city: 'client_city',
+    paymentTerms: 14,
+    companyId: 'company_1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+const Template: ComponentStory<typeof ClientsTable> = (args) => (
+  <div className="w-full h-full bg-violet-50 items-center justify-center p-10">
+    <ClientsTable {...args} clients={clients} />
+  </div>
+);
+
+export const Default = Template.bind({});
---
+++ app/src/components/ClientsTable.tsx
@@ -0,0 +1,176 @@
+import { useCallback, useMemo, useState } from 'react';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+  type FilterFn,
+} from '@tanstack/react-table';
+import { rankItem } from '@tanstack/match-sorter-utils';
+import {
+  faMagnifyingGlass,
+  faPencil,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useDisclosureForId from '@lib/useDisclosureForId';
+import Routes from '@lib/routes';
+import type { Customer } from '@server/customers/types';
+import {
+  Body,
+  BodyCell,
+  BodyRow,
+  getSortingIcon,
+  Head,
+  HeaderCell,
+  HeaderRow,
+  Table,
+} from './Table';
+import Button from './Button';
+import TextField from './TextField';
+import ConfirmationDialog from './ConfirmationDialog';
+import LinkButton from './LinkButton';
+
+const columnHelper = createColumnHelper<Customer>();
+
+const fuzzyFilter: FilterFn<Customer> = (row, columnId, value, addMeta) => {
+  const itemRank = rankItem(row.getValue(columnId), value);
+  addMeta({ itemRank });
+  return itemRank.passed;
+};
+
+type ClientsTableProps = {
+  clients: Customer[];
+  onDelete: (id: string) => void;
+};
+
+const fullName = (
+  firstName: string | undefined | null,
+  lastName: string | undefined | null
+) => [firstName, lastName].filter((part) => part).join(' ');
+
+const ClientsTable = ({ clients, onDelete }: ClientsTableProps) => {
+  const { isOpen, openFor, onClose, onOpen } = useDisclosureForId();
+  const handleDelete = useCallback(() => {
+    if (openFor) {
+      onDelete(openFor);
+    }
+  }, [onDelete, openFor]);
+  const [globalFilter, setGlobalFilter] = useState('');
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('name', {
+        header: () => 'Name',
+        cell: (info) => (
+          <div className="flex flex-col gap-1">
+            <div className="text-xl font-medium">{info.getValue()}</div>
+            <div className="text-base font-normal">
+              {fullName(
+                info.row.original.firstName,
+                info.row.original.lastName
+              )}
+            </div>
+          </div>
+        ),
+      }),
+      columnHelper.display({
+        id: 'actions',
+        cell: (info) => (
+          <div className="flex gap-4 justify-end">
+            <Button
+              color="danger"
+              endIcon={faTrash}
+              onClick={() => onOpen(info.row.original.id)}
+            >
+              Delete
+            </Button>
+            <LinkButton
+              color="secondary"
+              endIcon={faPencil}
+              href={Routes.client(info.row.original.id)}
+            >
+              Edit
+            </LinkButton>
+          </div>
+        ),
+      }),
+    ],
+    [onOpen]
+  );
+
+  const table = useReactTable({
+    data: clients,
+    columns,
+    state: {
+      globalFilter,
+    },
+    globalFilterFn: fuzzyFilter,
+    onGlobalFilterChange: setGlobalFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+
+  return (
+    <div className="flex flex-col w-full items-end">
+      <div className="w-full md:max-w-[250px]">
+        <TextField
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Search"
+          startAdornment={<FontAwesomeIcon icon={faMagnifyingGlass} />}
+        />
+      </div>
+      <Table>
+        <Head>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <HeaderRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <HeaderCell key={header.id}>
+                  {header.index < headerGroup.headers.length - 1 ? (
+                    <Button
+                      color="secondary"
+                      variant="borderless"
+                      size="sm"
+                      endIcon={getSortingIcon(header.column.getIsSorted())}
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                    </Button>
+                  ) : null}
+                </HeaderCell>
+              ))}
+            </HeaderRow>
+          ))}
+        </Head>
+        <Body>
+          {table.getRowModel().rows.map((row) => (
+            <BodyRow key={row.id}>
+              {row.getVisibleCells().map((cell, index) => (
+                <BodyCell key={cell.id} header={index === 0}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </BodyCell>
+              ))}
+            </BodyRow>
+          ))}
+        </Body>
+      </Table>
+      <ConfirmationDialog
+        isOpen={isOpen}
+        onClose={onClose}
+        onConfirm={handleDelete}
+        title="Are you sure you want to delete the client?"
+        description="This action can not be undone. You will have to add the client's details again if you want to work with them in the future"
+        confirm={{ label: 'Delete', icon: faTrash }}
+        cancel={{ label: 'Cancel' }}
+      />
+    </div>
+  );
+};
+
+export default ClientsTable;
