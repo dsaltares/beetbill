7dc7ac03b70524eddc454ddf9e873f94f485552c
David Saltares <david.saltares@gmail.com>
2022-10-26 14:16:14 +0200

    feat: 137 - add getInvoices procedure

---
+++ app/src/server/invoices/getInvoices.ts
@@ -0,0 +1,43 @@
+import { type Procedure, procedure } from '@server/trpc';
+import prisma from '@server/prisma';
+import { GetInvoicesInput, GetInvoicesOutput } from './types';
+import mapInvoiceEntity from './mapInvoiceEntity';
+
+export const getInvoices: Procedure<
+  GetInvoicesInput,
+  GetInvoicesOutput
+> = async ({ ctx: { session } }) => {
+  const invoices = await prisma.invoice.findMany({
+    where: {
+      deletedAt: null,
+      companyState: { companyId: session?.companyId as string },
+    },
+    include: {
+      companyState: {
+        include: {
+          company: true,
+        },
+      },
+      clientState: {
+        include: {
+          client: true,
+        },
+      },
+      items: {
+        include: {
+          productState: {
+            include: {
+              product: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  return invoices.map(mapInvoiceEntity);
+};
+
+export default procedure
+  .input(GetInvoicesInput)
+  .output(GetInvoicesOutput)
+  .query(getInvoices);
