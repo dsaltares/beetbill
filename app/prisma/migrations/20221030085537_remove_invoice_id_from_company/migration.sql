4b51b66360247c5c6832921085add86479012d70
David Saltares <david.saltares@gmail.com>
2022-10-30 11:01:58 +0200

    fix: remove bogus invoiceId column in Company model

---
+++ app/prisma/migrations/20221030085537_remove_invoice_id_from_company/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `invoiceId` on the `Company` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Company" DROP COLUMN "invoiceId";
