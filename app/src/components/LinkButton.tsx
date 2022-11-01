83995352a92969db1f92dbd1f1b44a6c8fe938be
David Saltares <david.saltares@gmail.com>
2022-11-01 15:02:55 +0200

    chore: 142 - upgrade to Next.js 13 (#155)
    * chore: 142 - upgrade to Next.js 13
    
    * fix: 142 - tests

--- app/src/components/LinkButton.tsx
+++ app/src/components/LinkButton.tsx
@@ -27,39 +27,37 @@ const LinkButton = ({
   href,
   ...anchorProps
 }: ButtonProps) => (
-  <Link href={href}>
-    <a
-      className={cn(
-        'relative flex items-center justify-center rounded-md text-base focus-ring text-left',
-        {
-          'bg-violet-700 text-zinc-50 hover:bg-violet-900':
-            color === 'primary' && variant === 'solid',
-          'bg-violet-100 text-violet-900 hover:bg-violet-300':
-            color === 'primary' && variant === 'light',
-          'bg-white text-violet-800 border border-violet-800 hover:bg-violet-100':
-            color === 'primary' && variant === 'outlined',
-          'bg-transparent text-violet-800 underline hover:bg-violet-100':
-            color === 'primary' && variant === 'borderless',
-          'bg-zinc-800 text-zinc-50 hover:bg-zinc-900':
-            color === 'secondary' && variant === 'solid',
-          'bg-zinc-200 text-zinc-800 hover:bg-zinc-400':
-            color === 'secondary' && variant === 'light',
-          'bg-white text-zinc-800 border border-zinc-800 hover:bg-zinc-200':
-            color === 'secondary' && variant === 'outlined',
-          'bg-transparent text-zinc-800 underline hover:bg-zinc-200':
-            color === 'secondary' && variant === 'borderless',
-          'py-1.5 px-3': size === 'sm',
-          'py-2 px-4': size === 'md',
-          'py-2.5 px-5': size === 'lg',
-          'w-full': fullWidth,
-        }
-      )}
-      {...anchorProps}
-    >
+  <Link
+    href={href}
+    className={cn('relative flex rounded-md text-base focus-ring text-left', {
+      'bg-violet-700 text-zinc-50 hover:bg-violet-900':
+        color === 'primary' && variant === 'solid',
+      'bg-violet-100 text-violet-900 hover:bg-violet-300':
+        color === 'primary' && variant === 'light',
+      'bg-white text-violet-800 border border-violet-800 hover:bg-violet-100':
+        color === 'primary' && variant === 'outlined',
+      'bg-transparent text-violet-800 underline hover:bg-violet-100':
+        color === 'primary' && variant === 'borderless',
+      'bg-zinc-800 text-zinc-50 hover:bg-zinc-900':
+        color === 'secondary' && variant === 'solid',
+      'bg-zinc-200 text-zinc-800 hover:bg-zinc-400':
+        color === 'secondary' && variant === 'light',
+      'bg-white text-zinc-800 border border-zinc-800 hover:bg-zinc-200':
+        color === 'secondary' && variant === 'outlined',
+      'bg-transparent text-zinc-800 underline hover:bg-zinc-200':
+        color === 'secondary' && variant === 'borderless',
+      'py-1.5 px-3': size === 'sm',
+      'py-2 px-4': size === 'md',
+      'py-2.5 px-5': size === 'lg',
+      'w-full': fullWidth,
+    })}
+    {...anchorProps}
+  >
+    <div className="flex items-center justify-center">
       <Icon className="mr-2" icon={startIcon} />
       {children}
       <Icon className="ml-2" icon={endIcon} />
-    </a>
+    </div>
   </Link>
 );
 
--- app/src/components/Sidebar/NavLink.tsx
+++ app/src/components/Sidebar/NavLink.tsx
@@ -12,19 +12,17 @@ type NavLinkProps = {
 
 const NavLink = ({ label, icon, href, selected }: NavLinkProps) => (
   <li className="list-none">
-    <Link href={href}>
-      <a
-        className={cn(
-          'flex items-center p-4 rounded-xl gap-4 text-white text-base focus-ring',
-          {
-            'bg-violet-900': !selected,
-            'bg-violet-700': selected,
-          }
-        )}
-      >
+    <Link
+      href={href}
+      className={cn('flex p-4 rounded-xl text-white text-base focus-ring', {
+        'bg-violet-900': !selected,
+        'bg-violet-700': selected,
+      })}
+    >
+      <div className="flex items-center gap-4">
         <FontAwesomeIcon className="w-4 h-4" icon={icon} />
         {label}
-      </a>
+      </div>
     </Link>
   </li>
 );
