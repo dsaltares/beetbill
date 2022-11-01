61bbb8ecaf9418772a0b598efab534be50d92ee2
David Saltares <david.saltares@gmail.com>
2022-11-01 22:29:25 +0200

    feat: 168 - add privacy policy and terms and conditions docs (#169)
    * feat: 168 - add privacy policy and terms and conditions docs
    
    * feat: 168 - add cookie policy
    
    * feat: 168 - add cookie consent banner
    
    * fix: 168 - linter in _app

--- app/src/pages/_app.next.tsx
+++ app/src/pages/_app.next.tsx
@@ -5,6 +5,7 @@ import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps as BaseAppProps } from 'next/app';
 import type { Session } from 'next-auth';
 import Head from 'next/head';
+import Script from 'next/script';
 import Providers from '@components/Providers';
 import Layout from '@components/Layout';
 import AppName from '@lib/appName';
@@ -25,6 +26,17 @@ const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
       </Head>
       <Component {...pageProps} />
     </Layout>
+    <Script
+      id="cookie-banner"
+      src="https://cdn.websitepolicies.io/lib/cookieconsent/cookieconsent.min.js"
+      defer
+    />
+    <Script
+      id="cookie-banner-init"
+      dangerouslySetInnerHTML={{
+        __html: `window.addEventListener("load",function(){window.wpcc.init({"border":"thin","corners":"small","colors":{"popup":{"background":"#F5F3FF","text":"#000000","border":"#6D28D9"},"button":{"background":"#6D28D9","text":"#ffffff"}},"position":"bottom","content":{"href":"https://invoicing.saltares.com/invoicing_cookie_policy.pdf","message":"This app uses cookies to ensure the best possible experience."}})});`,
+      }}
+    />
   </Providers>
 );
 
