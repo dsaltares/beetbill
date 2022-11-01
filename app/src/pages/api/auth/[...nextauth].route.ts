96c1b8cdaae3647a8faefc02652b13654beb03af
David Saltares <david.saltares@gmail.com>
2022-11-01 22:53:48 +0200

    feat: 171 - google auth

--- app/src/pages/api/auth/[...nextauth].route.ts
+++ app/src/pages/api/auth/[...nextauth].route.ts
@@ -1,6 +1,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
 import prisma from '@server/prisma';
 import { sendVerificationRequest } from '@server/auth/email';
 import events from '@server/auth/events';
@@ -20,6 +21,14 @@ export default NextAuth({
       from: process.env.EMAIL_FROM,
       sendVerificationRequest,
     }),
+    ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
+      ? [
+          GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          }),
+        ]
+      : []),
   ],
   secret: process.env.NEXT_AUTH_SECRET,
   callbacks: {
