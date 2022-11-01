9837b49e8fadef81d81f25b2ad1542109c96f378
David Saltares <david.saltares@gmail.com>
2022-11-01 10:40:29 +0200

    chore: 142 - upgrade to Next.js 13

--- app/src/components/Sidebar/NavLink.tsx
+++ app/src/components/Sidebar/NavLink.tsx
@@ -12,19 +12,18 @@ type NavLinkProps = {
 
 const NavLink = ({ label, icon, href, selected }: NavLinkProps) => (
   <li className="list-none">
-    <Link href={href}>
-      <a
-        className={cn(
-          'flex items-center p-4 rounded-xl gap-4 text-white text-base focus-ring',
-          {
-            'bg-violet-900': !selected,
-            'bg-violet-700': selected,
-          }
-        )}
-      >
-        <FontAwesomeIcon className="w-4 h-4" icon={icon} />
-        {label}
-      </a>
+    <Link
+      href={href}
+      className={cn(
+        'flex items-center p-4 rounded-xl gap-4 text-white text-base focus-ring',
+        {
+          'bg-violet-900': !selected,
+          'bg-violet-700': selected,
+        }
+      )}
+    >
+      <FontAwesomeIcon className="w-4 h-4" icon={icon} />
+      {label}
     </Link>
   </li>
 );
