eec213601092721dccad9dfbe3a18c936b25a332
David Saltares <david.saltares@gmail.com>
2022-11-02 22:46:53 +0200

    fix: 167 - 404 tests

--- app/src/pages/404.page.test.tsx
+++ app/src/pages/404.page.test.tsx
@@ -1,10 +1,18 @@
 import 'next';
-import { screen, render } from '@lib/testing';
+import { setupServer } from 'msw/node';
+import { screen, render, mockSession } from '@lib/testing';
 import NotFoundPage from './404.page';
+
+const session = undefined;
+const server = setupServer();
+
+beforeAll(() => server.listen());
+beforeEach(() => server.resetHandlers(mockSession(session)));
+afterAll(() => server.close());
 
 describe('NotFoundPage', () => {
   it('renders the page', async () => {
-    render(<NotFoundPage />);
+    render(<NotFoundPage />, { session });
 
     await screen.findByRole('link', { name: 'Home' });
   });
