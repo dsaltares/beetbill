cb420ca85a15b99513ad256d642f6dd52fd23144
David Saltares <david.saltares@gmail.com>
2022-11-14 10:48:49 +0100

    chore: 201 - make toggle align with textfield

--- app/src/components/Fields/Toggle.tsx
+++ app/src/components/Fields/Toggle.tsx
@@ -20,13 +20,13 @@ const Toggle = ({
 }: ToggleProps) => (
   <label
     htmlFor={id}
-    className={cn('flex flex-col text-sm', {
+    className={cn('flex flex-col text-sm font-medium', {
       'text-zinc-900 cursor-pointer': !disabled,
       'text-zinc-400 cursor-not-allowed': disabled,
     })}
   >
-    <div className="mb-2">{label}</div>
-    <div className="relative my-1.5">
+    <div className="mb-2 leading-6">{label}</div>
+    <div className="relative my-[5px]">
       <input
         type="checkbox"
         value=""
@@ -39,7 +39,7 @@ const Toggle = ({
       />
       <div
         className={cn(
-          "w-11 h-6 bg-zinc-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-violet-500 peer-focus:ring-offset-0 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-zinc-50 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:border-zinc-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-violet-700",
+          "w-[54px] h-7 bg-zinc-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-violet-500 peer-focus:ring-offset-0 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-zinc-50 after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:border-zinc-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all peer-checked:bg-violet-700",
           {
             'after:bg-zinc-50': !disabled,
             'after:bg-zinc-100': !!disabled,
