9cdea38fd36740f5028a238c837bafd8deecce77
David Saltares <david.saltares@gmail.com>
2022-11-20 11:53:52 +0200

    feat: 125 - getClients returns toBePaid and paid

--- app/src/server/clients/getClients.ts
+++ app/src/server/clients/getClients.ts
@@ -1,5 +1,7 @@
 import { type Procedure, procedure } from '@server/trpc';
 import prisma from '@server/prisma';
+import { getInvoices } from '@server/invoices/getInvoices';
+import type { Invoice } from '@server/invoices/types';
 import { GetClientsOutput } from './types';
 import mapClientEntity from './mapClientEntity';
 
@@ -18,7 +20,17 @@ export const getClients: Procedure<unknown, GetClientsOutput> = async ({
       },
     },
   });
-  return clients.map(mapClientEntity);
+  const invoices = await getInvoices({ ctx, input: {} });
+  const invoicesByClientId = invoices.reduce(
+    (acc, invoice) => ({
+      ...acc,
+      [invoice.client.id]: [...(acc[invoice.client.id] || []), invoice],
+    }),
+    {} as Record<string, Invoice[]>
+  );
+  return clients.map((client) =>
+    mapClientEntity(client, invoicesByClientId[client.id] || [])
+  );
 };
 
 export default procedure.output(GetClientsOutput).query(getClients);
