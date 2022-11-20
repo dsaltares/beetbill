c40f85ffdffa5531fe4ffd93976d31da9bce4fad
David Saltares <david.saltares@gmail.com>
2022-11-20 11:32:58 +0200

    fix: 224 - correct bg colour in full screen spinner

--- app/src/components/Layout/FullScreenSpinner.tsx
+++ app/src/components/Layout/FullScreenSpinner.tsx
@@ -1,7 +1,7 @@
 import Spinner from '@components/Spinner';
 
 const FullScreenSpinner = () => (
-  <div className="w-full h-full flex items-center justify-center">
+  <div className="w-full h-full flex items-center justify-center bg-violet-50 ">
     <Spinner size="lg" />
   </div>
 );
