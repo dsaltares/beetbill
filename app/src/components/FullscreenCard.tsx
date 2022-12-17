3b40e227a0542e19d71831fd7d00981102f043b2
David Saltares <david.saltares@gmail.com>
2022-12-17 18:49:46 +0200

    chore: adjust max width of FullScreenCard

--- app/src/components/FullscreenCard.tsx
+++ app/src/components/FullscreenCard.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 
 const FullScreenCard = ({ children }: PropsWithChildren) => (
   <div className="flex w-full justify-center items-center">
-    <div className="flex border rounded-lg border-zinc-300 bg-zinc-50 w-full lg:max-w-[900px] p-4 lg:p-8">
+    <div className="flex border rounded-lg border-zinc-300 bg-zinc-50 w-full lg:max-w-[980px] p-4 lg:p-8">
       {children}
     </div>
   </div>
