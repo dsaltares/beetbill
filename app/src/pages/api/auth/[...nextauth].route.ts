8d2b78cbfd65a1031f3fd9746e05fd871c26a1d1
David Saltares <david.saltares@gmail.com>
2022-12-28 11:41:59 +0200

    feat: allow account linking

--- app/src/pages/api/auth/[...nextauth].route.ts
+++ app/src/pages/api/auth/[...nextauth].route.ts
@@ -27,6 +27,7 @@ export default NextAuth({
           GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
           }),
         ]
       : []),
