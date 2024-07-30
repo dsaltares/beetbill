77d67f6f98f8cd18c13c9feed8a62e76814fec30
David Saltares <david.saltares@gmail.com>
2024-07-30 15:57:39 +0100

    fix: build

--- app/src/server/createContext.ts
+++ app/src/server/createContext.ts
@@ -1,10 +1,11 @@
 import type { inferAsyncReturnType } from '@trpc/server';
 import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
+import type { Session } from 'next-auth';
 import { getServerSession } from 'next-auth';
 import authOptions from './auth/authOptions';
 
 const createContext = async ({ req, res }: CreateNextContextOptions) => ({
-  session: await getServerSession(req, res, authOptions),
+  session: (await getServerSession(req, res, authOptions)) as Session | null,
 });
 
 export default createContext;
