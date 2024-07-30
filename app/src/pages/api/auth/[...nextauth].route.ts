76d6cb2a9728067c51946492a548a60c89410556
David Saltares <david.saltares@gmail.com>
2024-07-30 15:49:14 +0100

    fix: unable to login

--- app/src/pages/api/auth/[...nextauth].route.ts
+++ app/src/pages/api/auth/[...nextauth].route.ts
@@ -1,43 +1,4 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
-import GoogleProvider from 'next-auth/providers/google';
-import prisma from '@server/prisma';
-import { sendVerificationRequest } from '@server/auth/email';
-import events from '@server/auth/events';
-import callbacks from '@server/auth/callbacks';
+import authOptions from '@server/auth/authOptions';
 
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: parseInt(process.env.EMAIL_SERVER_PORT || '1025'),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      sendVerificationRequest,
-    }),
-    ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
-      ? [
-          GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            allowDangerousEmailAccountLinking: true,
-          }),
-        ]
-      : []),
-  ],
-  secret: process.env.NEXT_AUTH_SECRET,
-  callbacks,
-  events,
-  pages: {
-    signIn: '/auth/signin',
-    verifyRequest: '/auth/verify',
-    error: '/auth/error',
-  },
-});
+export default NextAuth(authOptions);
---
+++ app/src/server/auth/authOptions.ts
@@ -0,0 +1,44 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import EmailProvider from 'next-auth/providers/email';
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from '@server/prisma';
+import { sendVerificationRequest } from './email';
+import callbacks from './callbacks';
+import events from './events';
+
+const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: parseInt(process.env.EMAIL_SERVER_PORT || '1025'),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+      sendVerificationRequest,
+    }),
+    ...(process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET
+      ? [
+          GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
+          }),
+        ]
+      : []),
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+  callbacks,
+  events,
+  pages: {
+    signIn: '/auth/signin',
+    verifyRequest: '/auth/verify',
+    error: '/auth/error',
+  },
+};
+
+export default authOptions;
